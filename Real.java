f3a9b6d5530bf684526d2cbf3de182492ff05902
ElielTomaz <146689565+ElielTomaz@users.noreply.github.com>
2023-10-02 00:10:57 -0300

    Create Real.java

---
+++ Real.java
@@ -0,0 +1,34 @@
+package AtividadePratica;
+
+public class Real extends Moeda {
+
+	public Real(double valor){
+	       this.valor = valor;
+}
+	
+	@Override
+	public void info() {
+		System.out.println("Real - "+ valor);
+	}
+	
+	@Override
+	public double converter() {
+		return this.valor;
+	}
+	
+	//Comparação entre objetos
+	@Override
+	public boolean equals(Object objeto) {
+		if(this.getClass() != objeto.getClass()) {
+			return false;
+		}
+		
+		Real objetoDeReal = (Real) objeto;
+		
+		if(this.valor != objetoDeReal.valor) {
+			return false;
+		}
+		
+		return true;
+	}
+}
